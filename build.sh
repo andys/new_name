b5f0ca9c3d0c69b99400dfd192debec1162620d2
Andrew Snow <andrew@modulus.org>
2025-05-01 21:18:25 +1000

    fix build

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 goimports -w=true $(find . -type f -name '*.go' -not -path './vendor/*' )
 go mod tidy
 
-rm -f release/*/*
-mkdir -p release/amd64 release/arm64
-CGO_ENABLED=0 GOARCH=amd64 go build -ldflags="-s -w" -o release/amd64/anonymizer -v ./cmd/anonymizer
-CGO_ENABLED=0 GOARCH=arm64 go build -ldflags="-s -w" -o release/arm64/anonymizer -v ./cmd/anonymizer
+mkdir -p release
+rm -f release/*
+
+CGO_ENABLED=0 GOARCH=amd64 go build -ldflags="-s -w" -o release/anonymizer.amd64 -v ./cmd/anonymizer
+CGO_ENABLED=0 GOARCH=arm64 go build -ldflags="-s -w" -o release/anonymizer.arm64 -v ./cmd/anonymizer
