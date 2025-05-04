51fafbd8d7b3177c2f3f79292fd1ce27026fc06a
Andrew Snow <andrew@modulus.org>
2025-05-04 20:59:07 +1000

    resize worker queue

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ go mod tidy
 mkdir -p release
 rm -f release/*
 
-CGO_ENABLED=0 GOARCH=amd64 go build -ldflags="-s -w" -o release/new_name.amd64 -v ./cmd/new_name
-CGO_ENABLED=0 GOARCH=arm64 go build -ldflags="-s -w" -o release/new_name.arm64 -v ./cmd/new_name
+CGO_ENABLED=0 GOARCH=amd64 go build -ldflags="-s -w" -o release/new_names.amd64 -v ./cmd/new_names
+CGO_ENABLED=0 GOARCH=arm64 go build -ldflags="-s -w" -o release/new_names.arm64 -v ./cmd/new_names
